e63b8c655b64856366b8bda55469a193c7d1e73e
cristiandi <cristiandavidippolito@gmail.com>
2023-03-30 00:41:35 +0000

    feat: finally rendering chinese

--- src/modules/pdf/pdf.service.ts
+++ src/modules/pdf/pdf.service.ts
@@ -24,7 +24,7 @@ export class PdfService {
     const {html} = input;
 
     // Create a browser instance
-    const browser = await puppeteer.launch({ ignoreDefaultArgs: ['--disable-extensions'] });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
 
     // Create a new page
     const page = await browser.newPage();
